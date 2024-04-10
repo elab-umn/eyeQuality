533e232c055932c9540c0780770b8047c0765865
glick094 <glick094@umn.edu>
2024-04-09 19:47:10 -0500

    add function to parse_preprocessing_batch_summary which extracts data from output files into a R data frame

---
+++ R/parse_preprocessing_batch_summary.R
@@ -0,0 +1,73 @@
+# parse preprocessing_batch_summary files
+#' Title
+#'
+#' @param batch_summary_file filepath to preprocessing_batch_summary.tsv file
+#' @param info_to_extract string with value of "summary" | "failedfiles" | "successfulfiles"
+#'
+#' @return data dataframe exported data from preprocessing_batch_summary
+#' @export
+#'
+#' @importFrom readr read_tsv
+#' @importFrom stringr str_glue
+#' @importFrom stringr str_extract
+#' @importFrom dplyr mutate
+#'
+#'
+parse_preprocessing_batch_summary <-
+  function(batch_summary_file, info_to_extract = "summary") {
+    if (info_to_extract == "summary") {
+      read_tsv(
+        str_glue(batch_summary_file),
+        col_names = FALSE,
+        show_col_types = FALSE
+      ) %>%
+        tail(n = 1) %>%
+        mutate(
+          directory = str_extract(.data[[1]], "directory: ([\\S]+),", group = 1),
+          datasize = as.numeric(
+            str_extract(.data[[1]], "data size \\(MB\\): ([\\S]+),", group = 1)
+          ),
+          nfiles = as.numeric(
+            str_extract(.data[[1]], "n \\(ET Files\\): ([\\S]+),", group = 1)
+          ),
+          nPreprocessed = as.numeric(
+            str_extract(.data[[1]], "n \\(preprocessed\\): ([\\S]+),", group = 1)
+          ),
+          nFailed = as.numeric(
+            str_extract(.data[[1]], "n \\(failed preprocessing\\): ([\\S]+),", group = 1)
+          ),
+          runDuration = stringr::str_extract(.data[[1]], "run duration: ([\\S\\s]+),", group = 1),
+          runTime = as.numeric(
+            str_extract(.data[[1]], "runtime \\(s\\): ([\\S]+)$", group = 1)
+          ),
+        ) %>%
+        select(
+          directory,
+          datasize,
+          nfiles,
+          nPreprocessed,
+          nFailed,
+          runDuration,
+          runTime
+        ) %>%
+        # mutate( #this is IBIS specific
+        #   dataset = stringr::str_extract(directory, "(\\S+)/(\\S+)/(\\S+)/(\\S+)", group = 1),
+        #   site = stringr::str_extract(directory, "(\\S+)/(\\S+)/(\\S+)/(\\S+)", group = 2),
+        #   task = stringr::str_extract(directory, "(\\S+)/(\\S+)/(\\S+)/(\\S+)", group = 3),
+        # ) %>%
+        # select(
+        #   dataset,
+        #   site,
+        #   task,
+        #   datasize,
+        #   nfiles,
+        #   nPreprocessed,
+        #   nFailed,
+        #   runDuration,
+        #   runTime
+        # ) %>%
+        return()
+    } elif(info_to_extract == "failedfiles") {
+
+    }
+  }
